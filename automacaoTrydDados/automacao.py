7f352c34d551fa7d9c0357bc463327d63c3b1215
Omar Pandini <mzo.pandini@gmail.com>
2022-07-27 06:57:15 -0300

    melhoria

--- automacaoTrydDados/automacao.py
+++ automacaoTrydDados/automacao.py
@@ -91,6 +91,9 @@ def importaArquivoBaseDados():
     ti.sleep(1)
     py.write('http://localhost/bolsaEstatistica/importaCsv.php')
     py.press('enter')
+    py.hotkey('ctrl','t')
+    py.write('http://localhost/bolsaEstatistica/agressaoEstatistica.php')
+    py.press('enter')
 
 
 primeiroArquivo()
